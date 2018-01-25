0b5a842759a81f306377e36caadfadcf44186adb
Delivator <delivatorgaming@gmail.com>
2018-01-25 17:55:33 +0100

    Added downloadBinary function

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const got = require("got");
+
+if (!fs.existsSync("./bin")) fs.mkdirSync("./bin");
+
+function donwloadBinary() {
+  const startTime = new Date().getTime();
+  const isWin = /^win/.test(process.platform);
+  let url, filePath;
+  if (isWin) {
+    url = "https://yt-dl.org/downloads/latest/youtube-dl.exe";
+    filePath = "./bin/youtube-dl.exe";
+  } else {
+    url = "https://yt-dl.org/downloads/latest/youtube-dl";
+    filePath = "./bin/youtube-dl";
+  }
+  got(url, {followRedirect: true, encoding: null})
+    .then(resp => {
+      fs.writeFileSync(filePath, resp.body);
+      console.log(`youtube-dl binary download finished! (${(new Date().getTime() - startTime)/1000}s)`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+donwloadBinary();
