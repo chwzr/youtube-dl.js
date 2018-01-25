f3d39a02ddbc04fc8f04abdb0c79adbe209b8b5a
Delivator <delivatorgaming@gmail.com>
2018-01-25 19:26:56 +0100

    Updated download script

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const fs = require("fs");
 const got = require("got");
 
-if (!fs.existsSync("./bin")) fs.mkdirSync("./bin");
-
-exports.downloadBinary = function() {
-  const startTime = new Date().getTime();
-  const isWin = /^win/.test(process.platform);
-  let url, filePath;
-  if (isWin) {
-    url = "https://yt-dl.org/downloads/latest/youtube-dl.exe";
-    filePath = "./bin/youtube-dl.exe";
-  } else {
-    url = "https://yt-dl.org/downloads/latest/youtube-dl";
-    filePath = "./bin/youtube-dl";
-  }
-  got(url, {followRedirect: true, encoding: null})
-    .then(resp => {
-      fs.writeFileSync(filePath, resp.body);
-      console.log(`youtube-dl binary download finished! (${(new Date().getTime() - startTime)/1000}s)`);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+exports.downloadBinary = () => {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(__dirname + "/bin")) fs.mkdirSync(__dirname + "/bin");
+    const startTime = new Date().getTime();
+    const isWin = /^win/.test(process.platform);
+    let url, filePath;
+    if (isWin) {
+      url = "https://yt-dl.org/downloads/latest/youtube-dl.exe";
+      filePath = "./bin/youtube-dl.exe";
+    } else {
+      url = "https://yt-dl.org/downloads/latest/youtube-dl";
+      filePath = "./bin/youtube-dl";
+    }
+    got(url, {followRedirect: true, encoding: null})
+      .then(resp => {
+        fs.writeFileSync(filePath, resp.body);
+        resolve((new Date().getTime() - startTime)/1000);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
