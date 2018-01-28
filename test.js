b04c41d017c923a51a6b53c484987c2560d1a835
Delivator <delivatorgaming@gmail.com>
2018-01-28 16:23:34 +0100

    Updated test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const ytdl = require("./index.js");
 
-let filename = new Date().getTime();
+let filename = `${new Date().getTime()}.%(ext)s`;
 
-ytdl("https://youtu.be/q5weS3aY-Qc", ["-o", filename + ".%(ext)s", "-x", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"])
+ytdl("https://youtu.be/q5weS3aY-Qc", ["-o", filename, "-x", "--audio-format=mp3", "--restrict-filenames", "--external-downloader=ffmpeg", "--audio-quality=96k"])
   .then(data => {
     console.log(data);
   })
